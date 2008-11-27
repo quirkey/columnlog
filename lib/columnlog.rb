41b01acea593af45afa099cb6bf7d6efff27da18
Aaron Quint <aaron@quirkey.com>
2008-11-27 13:26:59 -0500

    Fixing requires

--- lib/columnlog.rb
+++ lib/columnlog.rb
@@ -10,12 +10,12 @@ require 'twitter'
 
 %w{
   super_hash 
-  app 
   post
   column
   credentials 
-  app/twitter 
-  app/tumblrs
+  apps/base
+  apps/twitter 
+  apps/tumblrs
 }.each do |file|
     require File.join(File.dirname(__FILE__), 'columnlog', file)
   end

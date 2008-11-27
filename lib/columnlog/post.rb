cd19b35325a383352cb985abe1c55ef1f8578974
Aaron Quint <aaron@quirkey.com>
2008-11-27 15:44:12 -0500

    - Added app to Post. - Post#save delegates to App

--- lib/columnlog/post.rb
+++ lib/columnlog/post.rb
@@ -3,6 +3,7 @@ module Columnlog
     attr_accessor :title, :body, :url, :author, :posted_at
     
     def initialize(params = {})
+      @app   = params[:app]
       @title = params[:title]
       @body  = params[:body]
       @url   = params[:url]
@@ -10,6 +11,14 @@ module Columnlog
       @posted_at = params[:posted_at] ? Chronic.parse(params[:posted_at]) : Time.now
     end
     
+    
+    def save
+      app.post(self)
+    end
+    
+    def app
+      @app || raise("No app specified for post: #{self}")
+    end
     
   end
 end

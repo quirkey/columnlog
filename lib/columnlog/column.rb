f1f1792f27bd3fd0f98d8d435b3aef6557276dbd
Aaron Quint <aaron@quirkey.com>
2008-11-27 12:58:21 -0500

    Working Column[]

--- lib/columnlog/column.rb
+++ lib/columnlog/column.rb
@@ -1,5 +1,18 @@
 module Columnlog
   class Column < StaticModel::Base
+   
+    class << self
+      
+      def [](name_or_id)
+        case name_or_id
+        when String
+          self.find_by_name(name_or_id)
+        else
+          self.find(name_or_id.to_i)
+        end
+      end
+      
+    end
     
   end
 end

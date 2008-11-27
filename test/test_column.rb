ed79c7ebcaceb389da139ba45289e7baf91c21df
Aaron Quint <aaron@quirkey.com>
2008-11-27 16:05:01 -0500

    Specing out the shortcut method

--- test/test_column.rb
+++ test/test_column.rb
@@ -16,7 +16,7 @@ class TestColumn < Test::Unit::TestCase
         should "return a Column" do
           assert @column.is_a?(Column)
         end
-        
+
         should "load attributes for column" do
           assert_equal '*tw', @column.shortcut
         end
@@ -34,49 +34,63 @@ class TestColumn < Test::Unit::TestCase
         should "return a Column" do
           assert @column.is_a?(Column)
         end
-        
+
         should "load attributes for column" do
           assert_equal 'test', @column.settings.username
         end
-        
+
       end
-      
+
       context "shortcuts" do
         setup do
           @shortcuts = Column.shortcuts
         end
-        
+
         should "collect all available shortcuts as a hash" do
           assert @shortcuts.is_a?(Hash)
           assert_equal Column.count, @shortcuts.length
         end
-        
+
         should "have shortcuts as keys" do
           assert @shortcuts.keys.include?('*tw')
         end
-        
+
         should "map to ids for values" do
           assert @shortcuts.values.include?(1)
         end
       end
 
+      context "shortcut scan" do
+        context "when the shortcut is the beginning of the text block" do
+          setup do
+            
+          end
+        end
+        context "when there are no shortcuts" do
+
+        end
+        context "when the shortut is in the middle of the text" do
+
+        end
+      end
+
     end
-    
+
     context "a Column" do
       setup do
         @column = Column['twitter']
       end
-      
+
       should "return a settings as a superhash" do
         assert @column.settings
         assert @column.settings.is_a?(SuperHash)
         assert_equal 'test', @column.settings[:username]
       end
-      
+
       should "load shortcut as a string" do
         assert_equal '*tw', @column.shortcut
       end
     end
   end
-  
+
 end

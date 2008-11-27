1b787cb53db6a40dd27d35927e21805f6d032d8b
Aaron Quint <aaron@quirkey.com>
2008-11-27 13:55:44 -0500

    Speced out the API for Apps::Base

--- lib/columnlog/apps/base.rb
+++ lib/columnlog/apps/base.rb
@@ -1,11 +1,20 @@
 module Columnlog
   module Apps
     class Base
+      attr_reader :settings
       
-      def initialize(settings = {})
-        
+      def initialize(settings = SuperHash.new)
+        @settings = settings
       end
       
+      def post(params); end
+      def get(howmany = 10); end
+      
+      def to_post(element); end
+      
+      protected
+      def app; end
+    
     end
   end
 end

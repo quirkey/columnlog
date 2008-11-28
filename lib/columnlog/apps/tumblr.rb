7168327b31b815d3f3a8cf1f7ad002a0ef42c1fc
Aaron Quint <aaron@quirkey.com>
2008-11-28 00:30:41 -0500

    Tumblr < Base Removing mispelled file

--- lib/columnlog/apps/tumblr.rb
+++ lib/columnlog/apps/tumblr.rb
@@ -1,35 +1,24 @@
 module Columnlog
   module Apps
-    class Tumblr
-            
+    class Tumblr < Base
+
       def post(params)
       end
-      
+
       def get(howmany = 10)
         out = []
-        if self.authorized?
-          url = "http://mrb.tumblr.com/api/read"
-          parser = XML::Parser.new
-          parser.file = url
-          parsed = parser.parse
-          parsed.find('posts').collect{|x| x.find('post').collect{|x| out << x}}
-        end
+        # url = "http://mrb.tumblr.com/api/read"
+        # parser = XML::Parser.new
+        # parser.file = url
+        # parsed = parser.parse
+        # parsed.find('posts').collect{|x| x.find('post').collect{|x| out << x}}
         out
       end
-      
-      def authorized?
-        user = Columnlog::Credentials.get(:tumblr).username
-        pass = Columnlog::Credentials.get(:tumblr).password
-        
-        if !user.nil? && !pass.nil?
-          self.credential_load(user,pass)
-        end
+
+      def app
+        settings
       end
-      
-      def credential_load(user,pass)
-        @user ||= user
-      end
-      
+
     end
   end
 end

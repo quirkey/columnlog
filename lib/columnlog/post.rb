436d9ecda2a08a308ec5025c6a02239e2dbdb024
Aaron Quint <aaron@quirkey.com>
2008-11-27 23:24:38 -0500

    We actually wanted to set a column for a post

--- lib/columnlog/post.rb
+++ lib/columnlog/post.rb
@@ -3,7 +3,7 @@ module Columnlog
     attr_accessor :title, :body, :url, :author, :posted_at
     
     def initialize(params = {})
-      @app   = params[:app]
+      @column = params[:column]
       @title = params[:title]
       @body  = params[:body]
       @url   = params[:url]
@@ -11,13 +11,12 @@ module Columnlog
       @posted_at = params[:posted_at] ? Chronic.parse(params[:posted_at]) : Time.now
     end
     
-    
     def save
-      app.post(self)
+      column.post(self)
     end
     
-    def app
-      @app || raise("No app specified for post: #{self}")
+    def column
+      @column || raise("No column/app specified for post: #{self}")
     end
     
   end

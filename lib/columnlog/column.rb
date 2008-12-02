b04b2a8a5fbdb00e64ec17ab987207568c706627
Aaron Quint <aaron@quirkey.com>
2008-12-02 12:35:50 -0500

    Very simple way of caching using active_support/cache

--- lib/columnlog/column.rb
+++ lib/columnlog/column.rb
@@ -1,9 +1,9 @@
 module Columnlog
   class Column < StaticModel::Base
-   set_data_file File.join(Columnlog.root, 'config', 'columns.yml')
-   
+    set_data_file File.join(Columnlog.root, 'config', 'columns.yml')
+
     class << self
-      
+
       def [](name_or_id)
         case name_or_id
         when String
@@ -12,15 +12,15 @@ module Columnlog
           self.find(name_or_id.to_i)
         end
       end
-      
+
       def shortcuts
         map = {}
         Column.all.each do |column|
-          map[column.shortcut] = column.id
+          map[column.shortcut] = column.id if column.shortcut
         end
         map
       end
-      
+
       def shortcut_scan(text_block)
         return if text_block.nil?
         text_block = text_block.strip
@@ -29,15 +29,30 @@ module Columnlog
           return [Column[id], text_block.gsub(exp, '')] if text_block =~ exp
         end; nil
       end
-      
+
+    end
+
+    delegate :post, :get, :to => :app
+
+    def cache_key(with = "")
+      cache_for = self.cache_for.blank? ? 5.minutes : self.cache_for
+      "#{name}_#{with}/#{Time.now.to_i / cache_for.to_i}"
     end
     
-    delegate :post, :get, :to => :app
-    
+    def get(howmany = 10)
+      cache.fetch(cache_key(howmany)) do
+        app.get(howmany)
+      end
+    end
+
+    def cache
+      @cache ||= ActiveSupport::Cache::MemoryStore.new#('tmp/columnlog')
+    end
+
     def settings
       @settings ||= SuperHash.new(@attributes[:settings])
     end
-    
+
     def app
       @app ||= "Columnlog::Apps::#{app_type}".constantize.new(settings)
     end

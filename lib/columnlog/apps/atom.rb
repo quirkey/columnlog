871f24e75bd0a4e3232f5481213c65f9d9987820
Aaron Quint <aaron@quirkey.com>
2008-12-06 11:45:22 -0500

    - Adding an Atom app - changed gem_dependency? to support libs with different names then the gems

---
+++ lib/columnlog/apps/atom.rb
@@ -0,0 +1,40 @@
+module Columnlog
+  module Apps
+    class Atom < Base
+
+      # def post(content)
+      #   content = content.is_a?(Post) ? content.body : content
+      #   app.post(content)
+      # end
+
+      def get(how_many = nil)
+        super
+        puts app.entries.inspect
+        # out = []
+        # doc = app.xpath('/rss/channel/item').collect {|i| out << to_post(i) }
+        # out.first(how_many)
+      end
+
+      def app
+        @app ||= Atom::Feed.new(settings.url)
+      end
+      
+      protected
+      def to_post(item, other = {})
+        Post.new({:title => get_inner_element(item, 'title'),
+                  :body => get_inner_element(item, 'description'), 
+                  :author => get_inner_element(item, 'author') || get_inner_element(item, 'dc:creator'),
+                  :posted_at => get_inner_element(item, 'pubDate'),
+                  :url => get_inner_element(item, 'link')
+                  })
+      end
+      
+      def get_inner_element(item, element)
+        inner = item.at(element)
+        inner ? inner.inner_text : nil
+      end
+
+    end
+  end
+end
+
--- lib/gems.rb
+++ lib/gems.rb
@@ -1,9 +1,10 @@
 require 'rubygems'
 
-def gem_dependency(gem_name)
+def gem_dependency(gem_name, lib_name = nil)
   $missing_gems ||= []
+  lib_name ||= gem_name
   begin
-    require gem_name
+    require lib_name
   rescue MissingSourceFile => e
     $missing_gems << gem_name
   end
@@ -11,6 +12,7 @@ end
 
 def missing_gems?
   if $missing_gems && !$missing_gems.empty?
+    $missing_gems.uniq!
     puts "Sorry, you're missing some gems to run columnlog:"
     $missing_gems.each do |gem_name|
       puts gem_name
@@ -29,8 +31,10 @@ gem_dependency 'net/http'
 gem_dependency 'open-uri'
 gem_dependency 'twitter'
 gem_dependency 'nokogiri'
-
-gem_dependency 'vendor/flickr/lib/flickr'
+gem_dependency 'atom-tools', 'atom/entry'
+gem_dependency 'atom-tools', 'atom/collection'
+gem_dependency 'atom-tools', 'atom/feed'
+gem_dependency 'flickr', 'vendor/flickr/lib/flickr'
 
 missing_gems?
 
